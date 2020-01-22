8b8222940bc331ff99f3b82702719959328d0a47
Xin <frances.xliang@gmail.com>
2020-01-22 21:08:17 +1100

    Add table placeholder to app

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+import Table from 'react-bootstrap/Table';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+          <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Audio</th>
+                  <th>Annotation</th>
+                  <th>DeepSpeech</th>
+                  <th>Confidence</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+          </Table>
+      </div>
     </div>
   );
 }
