35c1b3d9c653e0a12a75faee4ab4da8747661ad6
Xin <frances.xliang@gmail.com>
2020-02-08 18:26:05 +1100

    Update style of project configs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import {InputGroup, FormControl} from 'react-bootstrap';
+import {InputGroup, FormControl, Accordion, Card, Button} from 'react-bootstrap';
 import FileUploader from "./FileUpload";
 
 
@@ -30,46 +30,59 @@ class App extends React.Component {
         <div className="App">
           <div className="container" style={{paddingTop: "1rem"}}>
               <div style={{marginBottom: "50px"}}>
-                <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)", fontWeight: "bold"}}>Project Name</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      name="projectName"
-                      onChange={e => this.handleTextChange(e)}
-                      aria-label="project-name"
-                      aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)", fontWeight: "bold"}}>Model Confidence Threshold</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                      name="modelThreshold"
-                      onChange={e => this.handleTextChange(e)}
-                      aria-label="threshold"
-                      aria-describedby="basic-addon2"
-                    />
-                </InputGroup>
-                <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)", fontWeight: "bold"}}>Run Google Speech-to-Text</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl disabled aria-label="" style={{backgroundColor: "rgb(217, 217, 217, 0.2)"}}/>
-                    <InputGroup.Append style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>
-                      <InputGroup.Checkbox name="isGoogleStt" onChange={e => this.handleCheckboxChange(e)}/>
-                    </InputGroup.Append>
-                </InputGroup>
-                <InputGroup className="mb-3">
-                    <InputGroup.Prepend>
-                      <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)", fontWeight: "bold"}}>Automate annotation</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl disabled aria-label="" style={{backgroundColor: "rgb(217, 217, 217, 0.2)"}}/>
-                    <InputGroup.Append>
-                      <InputGroup.Checkbox name="isAutomated" onChange={e => this.handleCheckboxChange(e)}/>
-                    </InputGroup.Append>
-                </InputGroup>
+                  <Accordion defaultActiveKey="" style={{borderBottom: "1px solid rgba(0, 0, 0, 0.125)", textAlign: "left"}}>
+                      <Card>
+                        <Card.Header>
+                          <Accordion.Toggle as={Button} variant="" eventKey="0" style={{fontWeight: "bold"}}>
+                            Project Configurations
+                          </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                          <Card.Body>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                  <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>Project Name</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl
+                                  name="projectName"
+                                  onChange={e => this.handleTextChange(e)}
+                                  aria-label="project-name"
+                                  aria-describedby="basic-addon1"
+                                />
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                  <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>Model Confidence Threshold</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl
+                                  name="modelThreshold"
+                                  onChange={e => this.handleTextChange(e)}
+                                  aria-label="threshold"
+                                  aria-describedby="basic-addon2"
+                                />
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                  <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>Run Google Speech-to-Text</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl disabled aria-label="" style={{backgroundColor: "rgb(217, 217, 217, 0.2)"}}/>
+                                <InputGroup.Append style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>
+                                  <InputGroup.Checkbox name="isGoogleStt" onChange={e => this.handleCheckboxChange(e)}/>
+                                </InputGroup.Append>
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                  <InputGroup.Text style={{backgroundColor: "rgb(217, 217, 217, 0.3)"}}>Automate annotation</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl disabled aria-label="" style={{backgroundColor: "rgb(217, 217, 217, 0.2)"}}/>
+                                <InputGroup.Append>
+                                  <InputGroup.Checkbox name="isAutomated" onChange={e => this.handleCheckboxChange(e)}/>
+                                </InputGroup.Append>
+                            </InputGroup>
+                          </Card.Body>
+                        </Accordion.Collapse>
+                      </Card>
+                  </Accordion>
               </div>
               <FileUploader />
           </div>
